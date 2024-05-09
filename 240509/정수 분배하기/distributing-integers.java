b585dc36dafca693d7bfda3daad1943bc0cbcefe
Barded <65287117+barded1998@users.noreply.github.com>
2024-05-09 09:34:48 +0900

    Title: 정수 분배하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240509/정수 분배하기/distributing-integers.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int n, m, k = -1;
+    static int[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		arr = new int[n];
+		for(int i = 0; i < n; i++){
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		int start =0, end = 100_001;
+		while(start <= end) {
+			int mid = (start + end) / 2;
+			if(calc(mid) >= m){
+				k = Math.max(k, mid);
+				start = mid + 1;
+			} else {
+				end = mid-1;
+			}
+		}
+
+		System.out.println(k);
+
+
+    }	
+
+	static int calc(int x){
+		int cnt = 0;
+		for(int i : arr){
+			cnt += i / x;
+		}
+		return cnt;
+	}
+
+}
